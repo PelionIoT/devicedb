ba712a8f21e6feaf0c8c1617f3b7a72a7611fee9
Travis McCollum <x85446@yahoo.com>
2020-01-13 09:54:34 -0600

    added MIT header to hacks/scripts folder

--- hack/generate-certs-and-identity.sh
+++ hack/generate-certs-and-identity.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Copyright (c) 2019 ARM Limited.
+#
+# SPDX-License-Identifier: MIT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 set -e
 
 if [ -z "$1" ]
@@ -34,54 +56,54 @@ echo $SITE_ID > $OUT_DIR/site_id
 
 # Generate device certificate CA
 openssl genrsa \
-    -des3 \
-    -out $OUT_DIR/myCA.key \
-    -passout pass:password \
-    2048
+-des3 \
+-out $OUT_DIR/myCA.key \
+-passout pass:password \
+2048
 
 openssl req \
-    -x509 \
-    -new -nodes \
-    -key $OUT_DIR/myCA.key \
-    -sha256 \
-    -days 1825 \
-    -passin pass:password \
-    -subj "/C=US/ST=TX/L=City/O=my_o/OU=my_ou/CN=root_ca/emailAddress=email@example.com" \
-    -out $OUT_DIR/myCA.pem
+-x509 \
+-new -nodes \
+-key $OUT_DIR/myCA.key \
+-sha256 \
+-days 1825 \
+-passin pass:password \
+-subj "/C=US/ST=TX/L=City/O=my_o/OU=my_ou/CN=root_ca/emailAddress=email@example.com" \
+-out $OUT_DIR/myCA.pem
 
 # Generate device certificates for devicedb edge node using CA
 openssl genrsa \
-    -out $OUT_DIR/client.key \
-    2048
+-out $OUT_DIR/client.key \
+2048
 
 openssl req \
-    -new \
-    -key $OUT_DIR/client.key \
-    -subj "/C=US/ST=TX/L=City/O=my_o/OU=$SITE_ID/CN=$DEVICE_ID/emailAddress=email@example.com" \
-    -passout pass:password \
-    -out $OUT_DIR/client.csr
+-new \
+-key $OUT_DIR/client.key \
+-subj "/C=US/ST=TX/L=City/O=my_o/OU=$SITE_ID/CN=$DEVICE_ID/emailAddress=email@example.com" \
+-passout pass:password \
+-out $OUT_DIR/client.csr
 
 openssl x509 \
-    -req \
-    -in $OUT_DIR/client.csr \
-    -CA $OUT_DIR/myCA.pem \
-    -CAkey $OUT_DIR/myCA.key \
-    -CAcreateserial \
-    -days 1825 \
-    -sha256 \
-    -extfile client.ext \
-    -passin pass:password \
-    -out $OUT_DIR/client.crt
+-req \
+-in $OUT_DIR/client.csr \
+-CA $OUT_DIR/myCA.pem \
+-CAkey $OUT_DIR/myCA.key \
+-CAcreateserial \
+-days 1825 \
+-sha256 \
+-extfile client.ext \
+-passin pass:password \
+-out $OUT_DIR/client.crt
 
 # Generate self-signed certificate for devicedb cloud node
 openssl req \
-    -x509 \
-    -newkey rsa:4096 \
-    -keyout $OUT_DIR/server.key \
-    -out $OUT_DIR/server.crt \
-    -subj "/C=US/ST=TX/L=City/O=my_o/OU=my_ou/CN=$CLOUD_HOST/emailAddress=email@example.com" \
-    -passout pass:password \
-    -days 365
+-x509 \
+-newkey rsa:4096 \
+-keyout $OUT_DIR/server.key \
+-out $OUT_DIR/server.crt \
+-subj "/C=US/ST=TX/L=City/O=my_o/OU=my_ou/CN=$CLOUD_HOST/emailAddress=email@example.com" \
+-passout pass:password \
+-days 365
 
 cp $OUT_DIR/myCA.pem $EDGE_CLIENT_RESOURCES/myCA.pem
 cp $OUT_DIR/client.crt $EDGE_CLIENT_RESOURCES/client.crt
