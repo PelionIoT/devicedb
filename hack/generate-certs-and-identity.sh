3d002580f63dfc4e7d54a6bb2367b3639340c053
Janne Kiiskilä <janne.kiiskila@izumanetworks.com>
2023-01-24 12:49:37 +0200

    pysh-checks to scripts under hack -folder

--- hack/generate-certs-and-identity.sh
+++ hack/generate-certs-and-identity.sh
@@ -32,7 +32,7 @@ then
     exit 1
 fi
 
-OUT_DIR=$1
+OUT_DIR="$1"
 
 if [ ! -d "$OUT_DIR" ]; then
     echo "Output directory $OUT_DIR does not exist"
@@ -40,75 +40,74 @@ if [ ! -d "$OUT_DIR" ]; then
     exit 1
 fi
 
-echo "$(cat devicedb.conf.tpl.yaml | envsubst)" > $OUT_DIR/devicedb.conf
+envsubst < devicedb.conf.tpl.yaml > "$OUT_DIR/devicedb.conf"
 
 if [ -f "$OUT_DIR/ready" ]; then
     echo "Identity already exists. Skipping identity generation"
-
     exit 0
 fi
 
-DEVICE_ID=`uuid | sed 's/-//g'`
-SITE_ID=`uuid | sed 's/-//g'`
+DEVICE_ID=$(uuid | sed 's/-//g')
+SITE_ID=$(uuid | sed 's/-//g')
 
 # Write the identity variables to files
-echo $DEVICE_ID > $OUT_DIR/device_id
-echo $SITE_ID > $OUT_DIR/site_id
+echo "$DEVICE_ID" > "$OUT_DIR/device_id"
+echo "$SITE_ID" > "$OUT_DIR/site_id"
 
 # Generate device certificate CA
 openssl genrsa \
     -des3 \
-    -out $OUT_DIR/myCA.key \
+    -out "$OUT_DIR/myCA.key" \
     -passout pass:password \
     2048
 
 openssl req \
     -x509 \
     -new -nodes \
-    -key $OUT_DIR/myCA.key \
+    -key "$OUT_DIR/myCA.key" \
     -sha256 \
     -days 1825 \
     -passin pass:password \
     -subj "/C=US/ST=TX/L=City/O=my_o/OU=my_ou/CN=root_ca/emailAddress=email@example.com" \
-    -out $OUT_DIR/myCA.pem
+    -out "$OUT_DIR/myCA.pem"
 
 # Generate device certificates for devicedb edge node using CA
 openssl genrsa \
-    -out $OUT_DIR/client.key \
+    -out "$OUT_DIR/client.key" \
     2048
 
 openssl req \
     -new \
-    -key $OUT_DIR/client.key \
+    -key "$OUT_DIR/client.key" \
     -subj "/C=US/ST=TX/L=City/O=my_o/OU=$SITE_ID/CN=$DEVICE_ID/emailAddress=email@example.com" \
     -passout pass:password \
-    -out $OUT_DIR/client.csr
+    -out "$OUT_DIR/client.csr"
 
 openssl x509 \
     -req \
-    -in $OUT_DIR/client.csr \
-    -CA $OUT_DIR/myCA.pem \
-    -CAkey $OUT_DIR/myCA.key \
+    -in "$OUT_DIR/client.csr" \
+    -CA "$OUT_DIR/myCA.pem" \
+    -CAkey "$OUT_DIR/myCA.key" \
     -CAcreateserial \
     -days 1825 \
     -sha256 \
     -extfile client.ext \
     -passin pass:password \
-    -out $OUT_DIR/client.crt
+    -out "$OUT_DIR/client.crt"
 
 # Generate self-signed certificate for devicedb cloud node
 openssl req \
     -x509 \
     -newkey rsa:4096 \
-    -keyout $OUT_DIR/server.key \
-    -out $OUT_DIR/server.crt \
+    -keyout "$OUT_DIR/server.key" \
+    -out "$OUT_DIR/server.crt" \
     -subj "/C=US/ST=TX/L=City/O=my_o/OU=my_ou/CN=$CLOUD_HOST/emailAddress=email@example.com" \
     -passout pass:password \
     -days 365
 
-cp $OUT_DIR/myCA.pem $EDGE_CLIENT_RESOURCES/myCA.pem
-cp $OUT_DIR/client.crt $EDGE_CLIENT_RESOURCES/client.crt
-cp $OUT_DIR/client.key $EDGE_CLIENT_RESOURCES/client.key
-cp $OUT_DIR/devicedb.conf $EDGE_CLIENT_RESOURCES/devicedb.conf
+cp "$OUT_DIR/myCA.pem" "$EDGE_CLIENT_RESOURCES/myCA.pem"
+cp "$OUT_DIR/client.crt" "$EDGE_CLIENT_RESOURCES/client.crt"
+cp "$OUT_DIR/client.key" "$EDGE_CLIENT_RESOURCES/client.key"
+cp "$OUT_DIR/devicedb.conf" "$EDGE_CLIENT_RESOURCES/devicedb.conf"
 
-touch $OUT_DIR/ready
+touch "$OUT_DIR/ready"
